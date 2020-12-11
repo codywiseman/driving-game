279149919fc53718dbe6b1ce76e2a8a0d3a99672
Cody Wiseman <cwiseman09@yahoo.com>
2020-12-11 13:26:32 -0800

    Car turns in direction of the arrow key pressed

--- driving game/main.js
+++ driving game/main.js
@@ -1,3 +1,20 @@
 var carDirection = {
 
 }
+
+var $car = document.querySelector('img');
+
+document.addEventListener('keydown', function(e) {
+  if(e.key === 'ArrowDown') {
+    $car.className = 'south';
+  }
+  if (e.key === 'ArrowLeft') {
+    $car.className = 'west';
+  }
+  if (e.key === 'ArrowUp') {
+    $car.className = 'north';
+  }
+  if (e.key === 'ArrowRight') {
+    $car.className = 'east';
+  }
+})
